fd18842e7faabef3e5c130611c715ea6986e18b2
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2024-10-17 14:59:01 +0300

    + NamesEnum

--- src/TypesEnum.php
+++ src/TypesEnum.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace IfCastle\TypeDefinitions;
 
-enum NamesEnum: string
+enum TypesEnum: string
 {
     case BOOL                       = 'bool';
     case INTEGER                    = 'integer';
