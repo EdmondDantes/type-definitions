65589b0629d1bbc1f2de51fd4b5e10db949199a0
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2024-11-14 08:54:41 +0200

    + encode method for TypeOneOf.php

--- src/TypeOneOf.php
+++ src/TypeOneOf.php
@@ -67,11 +67,25 @@ class TypeOneOf extends DefinitionAbstract
         return $this->cases;
     }
 
+    /**
+     * @throws DefinitionIsNotValid
+     */
     #[\Override]
     public function encode(mixed $data): mixed
     {
-        /* @phpstan-ignore-next-line */
-        // TODO: Implement arrayEncode() method.
+        if ($this->cases === []) {
+            throw new DefinitionIsNotValid($this, 'Enum types should be not empty');
+        }
+
+        foreach ($this->cases as $type) {
+            try {
+                return $type->encode($data);
+            } catch (DefinitionIsNotValid) {
+                continue;
+            }
+        }
+
+        throw new DefinitionIsNotValid($this, 'Enum types are not matched');
     }
 
     /**
